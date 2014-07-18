78124f8ee7328c641b706b7d875aab0ded299481
Chibisov Gennady <web-chib@yandex-team.ru>
2014-07-18 11:30:16 +0400

    Documented usage of nested router with generic relation fields. Fixes #21

--- tests_app/tests/functional/routers/nested_router_mixin/serializers.py
+++ tests_app/tests/functional/routers/nested_router_mixin/serializers.py
@@ -2,15 +2,18 @@
 from rest_framework import serializers
 
 from .models import (
-    NestedRouterMixinUserModel,
-    NestedRouterMixinGroupModel,
-    NestedRouterMixinPermissionModel,
+    NestedRouterMixinUserModel as UserModel,
+    NestedRouterMixinGroupModel as GroupModel,
+    NestedRouterMixinPermissionModel as PermissionModel,
+    NestedRouterMixinTaskModel as TaskModel,
+    NestedRouterMixinBookModel as BookModel,
+    NestedRouterMixinCommentModel as CommentModel,
 )
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = NestedRouterMixinUserModel
+        model = UserModel
         fields = (
             'id',
             'name'
@@ -19,7 +22,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
-        model = NestedRouterMixinGroupModel
+        model = GroupModel
         fields = (
             'id',
             'name'
@@ -28,8 +31,37 @@ class GroupSerializer(serializers.ModelSerializer):
 
 class PermissionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = NestedRouterMixinPermissionModel
+        model = PermissionModel
         fields = (
             'id',
             'name'
+        )
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskModel
+        fields = (
+            'id',
+            'title'
+        )
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookModel
+        fields = (
+            'id',
+            'title'
+        )
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommentModel
+        fields = (
+            'id',
+            'content_type',
+            'object_id',
+            'text'
         )
--- tests_app/tests/functional/routers/nested_router_mixin/views.py
+++ tests_app/tests/functional/routers/nested_router_mixin/views.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from django.contrib.contenttypes.models import ContentType
+
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
@@ -6,19 +8,25 @@ from rest_framework_extensions.mixins import NestedViewSetMixin
 from rest_framework_extensions.decorators import action, link
 
 from .models import (
-    NestedRouterMixinUserModel,
-    NestedRouterMixinGroupModel,
-    NestedRouterMixinPermissionModel,
+    NestedRouterMixinUserModel as UserModel,
+    NestedRouterMixinGroupModel as GroupModel,
+    NestedRouterMixinPermissionModel as PermissionModel,
+    NestedRouterMixinTaskModel as TaskModel,
+    NestedRouterMixinBookModel as BookModel,
+    NestedRouterMixinCommentModel as CommentModel
 )
 from .serializers import (
     UserSerializer,
     GroupSerializer,
     PermissionSerializer,
+    TaskSerializer,
+    BookSerializer,
+    CommentSerializer
 )
 
 
 class UserViewSet(NestedViewSetMixin, ModelViewSet):
-    model = NestedRouterMixinUserModel
+    model = UserModel
     serializer_class = UserSerializer
 
     @action(endpoint='users-list-action', is_for_list=True)
@@ -31,7 +39,7 @@ class UserViewSet(NestedViewSetMixin, ModelViewSet):
 
 
 class GroupViewSet(NestedViewSetMixin, ModelViewSet):
-    model = NestedRouterMixinGroupModel
+    model = GroupModel
     serializer_class = GroupSerializer
 
     @link(endpoint='groups-list-link', is_for_list=True)
@@ -44,7 +52,7 @@ class GroupViewSet(NestedViewSetMixin, ModelViewSet):
 
 
 class PermissionViewSet(NestedViewSetMixin, ModelViewSet):
-    model = NestedRouterMixinPermissionModel
+    model = PermissionModel
     serializer_class = PermissionSerializer
 
     @action(endpoint='permissions-list-action', is_for_list=True)
@@ -54,3 +62,32 @@ class PermissionViewSet(NestedViewSetMixin, ModelViewSet):
     @action(endpoint='permissions-action')
     def permissions_action(self, request, *args, **kwargs):
         return Response('permissions action')
+
+
+class TaskViewSet(NestedViewSetMixin, ModelViewSet):
+    model = TaskModel
+    serializer_class = TaskSerializer
+
+
+class BookViewSet(NestedViewSetMixin, ModelViewSet):
+    model = BookModel
+    serializer_class = BookSerializer
+
+
+class CommentViewSet(NestedViewSetMixin, ModelViewSet):
+    queryset = CommentModel.objects.all()
+    serializer_class = CommentSerializer
+
+
+class TaskCommentViewSet(CommentViewSet):
+    def get_queryset(self):
+        return super(TaskCommentViewSet, self).get_queryset().filter(
+            content_type=ContentType.objects.get_for_model(TaskModel)
+        )
+
+
+class BookCommentViewSet(CommentViewSet):
+    def get_queryset(self):
+        return super(BookCommentViewSet, self).get_queryset().filter(
+            content_type=ContentType.objects.get_for_model(BookModel)
+        )
