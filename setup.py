d3e4a4a061900e44106fe80cbb7417aea45a8cea
Gennady Chibisov <web-chib@ya.ru>
2014-10-13 17:37:59 +0400

    Added MIT license to setup.py

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
