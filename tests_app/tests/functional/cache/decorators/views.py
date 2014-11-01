a48fa536fdfb3674eed3fa6891dd7ea7ec49136f
Rob Terhaar <rob@atlanticdynamic.com>
2014-11-01 14:53:44 -0400

    add test for query parameter cache

--- tests_app/tests/functional/cache/decorators/views.py
+++ tests_app/tests/functional/cache/decorators/views.py
@@ -3,16 +3,35 @@ from rest_framework import views
 from rest_framework.response import Response
 
 from rest_framework_extensions.cache.decorators import cache_response
+from rest_framework_extensions.key_constructor.bits import (
+    QueryParamsKeyBit,
+)
+from rest_framework_extensions.key_constructor.constructors import (
+    DefaultKeyConstructor,
+)
+
+# This parameter is used by the params constructor and the HelloParamView view.
+# Both the constructor and the decorator must list which parameters are unique
+# to this cache key.
+TEST_PARAM = 'param'
+
+
+class _ParamsConstructor(DefaultKeyConstructor):
+    param_bit = QueryParamsKeyBit([TEST_PARAM])
 
 
 class HelloView(views.APIView):
+    """ normal test that uses the DefaultKeyConstructor
+    """
     @cache_response()
     def get(self, request, *args, **kwargs):
         return Response('Hello world')
 
 
 class HelloParamView(views.APIView):
-    @cache_response()
+    """ query parameter caching from the QueryParamsKeyBit key bit
+    """
+    @cache_response(key_func=_ParamsConstructor())
     def get(self, request, *args, **kwargs):
-        param = request.GET.get("param")
+        param = request.GET.get(TEST_PARAM)
         return Response('Hello ' + param)
