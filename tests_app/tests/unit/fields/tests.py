f8200c625c8f2921eed63564ec6246089bccc046
Anton-Shutik <anton.shutik@itechart-group.com>
2015-12-02 15:13:51 +0300

    added tests

---
+++ tests_app/tests/unit/fields/tests.py
@@ -0,0 +1,31 @@
+from rest_framework_extensions import fields
+
+
+class TestMappingFieldWithIntKeys(FieldValues):
+    """
+    Valid and invalid values for `MappingField`.
+    """
+    valid_inputs = {
+        'one': 1,
+        'two': 2,
+        3: 'three',
+        4: 'four'
+    }
+    invalid_inputs = {
+        5: ['"5" not found in "mapping" values'],
+        'abc': ['"abc" not found in "mapping" values']
+    }
+    outputs = {
+        1: 'one',
+        2: 'two',
+        'three': 3,
+        'four': 4
+    }
+    field = fields.MappingField(
+        mapping={
+            1: 'one',
+            2: 'two',
+            'three': 3,
+            'four': 4
+        }
+    )
