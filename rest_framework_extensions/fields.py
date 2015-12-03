0b98f089ad09b0bbb35674e5ebfa0d43b70a4b9f
Anton-Shutik <anton.shutik@itechart-group.com>
2015-12-03 11:36:18 +0300

    Added MappingField

--- rest_framework_extensions/fields.py
+++ rest_framework_extensions/fields.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from rest_framework.fields import Field
 from rest_framework.relations import HyperlinkedRelatedField
 
 
@@ -26,3 +27,37 @@ class ResourceUriField(HyperlinkedRelatedField):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('source', '*')
         super(ResourceUriField, self).__init__(*args, **kwargs)
+
+
+class MappingField(Field):
+    default_error_messages = {
+        'key_not_found': '"{value}" not found in "mapping" keys',
+        'value_not_found': '"{value}" not found in "mapping" values'
+    }
+
+    def __init__(self, mapping, reverse_mapping=None, **kwargs):
+        super(MappingField, self).__init__(**kwargs)
+
+        def check_dict(d):
+            assert isinstance(d, dict), '"mapping" should be a dictionary'
+            for k, v in d.items():
+                assert isinstance(k, (str, int, bool)) and isinstance(v, (str, int, bool)), '"mapping" can contain only simple types'
+
+        check_dict(mapping)
+
+        self.mapping = mapping
+        if reverse_mapping is None:
+            self.reverse_mapping = {v: k for k, v in mapping.iteritems()}
+        else:
+            check_dict(reverse_mapping)
+            self.reverse_mapping = reverse_mapping
+
+    def to_representation(self, value):
+        if value in self.mapping:
+            return self.mapping[value]
+        self.fail('key_not_found', value=value)
+
+    def to_internal_value(self, data):
+        if data in self.reverse_mapping:
+            return self.reverse_mapping[data]
+        self.fail('value_not_found', value=data)
