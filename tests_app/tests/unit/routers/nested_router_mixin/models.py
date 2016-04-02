66b465d1e14a581c4b41b3dffe473bbd13262166
auvipy <auvipy@gmail.com>
2016-04-02 15:09:08 +0600

    apps conflict fix

--- tests_app/tests/unit/routers/nested_router_mixin/models.py
+++ tests_app/tests/unit/routers/nested_router_mixin/models.py
@@ -6,17 +6,17 @@ class NestedRouterMixinPermissionModel(models.Model):
     name = models.CharField(max_length=10)
 
     class Meta:
-        app_label = 'tests_app'
+        app_label = 'permission_app'
         verbose_name = 'permission'
 
 
 class NestedRouterMixinGroupModel(models.Model):
     name = models.CharField(max_length=10)
     permissions = models.ManyToManyField(
-        'UnitNestedRouterMixinPermissionModel')
+        'NestedRouterMixinPermissionModel')
 
     class Meta:
-        app_label = 'tests_app'
+        app_label = 'group_app'
         verbose_name = 'group'
 
 
@@ -26,5 +26,5 @@ class NestedRouterMixinUserModel(models.Model):
         'NestedRouterMixinGroupModel', related_name='user_groups')
 
     class Meta:
-        app_label = 'tests_app'
+        app_label = 'user_app'
         verbose_name = 'user'
