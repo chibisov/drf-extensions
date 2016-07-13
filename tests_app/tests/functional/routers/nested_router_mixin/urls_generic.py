a337abc4b16e79cb3272c694fbf4c95d685968d0
Jaakko Kantojärvi <jaakko@n-1.fi>
2016-07-13 20:46:58 +0300

    Cleanup and fix router tests for django 1.8 & 1.9
    Following command doesn't show failures:
     tox -e django.1.9,django.1.8.lts -- \
       tests_app.tests.unit.routers \
       tests_app.tests.functional.routers

---
+++ tests_app/tests/functional/routers/nested_router_mixin/urls_generic.py
@@ -0,0 +1,26 @@
+from django.conf.urls import url, include
+from rest_framework_extensions.routers import ExtendedSimpleRouter
+
+from .views import (
+    TaskViewSet,
+    TaskCommentViewSet,
+    BookViewSet,
+    BookCommentViewSet
+)
+
+router = ExtendedSimpleRouter()
+
+# tasks route
+(
+    router.register(r'tasks', TaskViewSet)
+            .register(r'comments', TaskCommentViewSet, 'tasks-comment', parents_query_lookups=['object_id'])
+)
+# books route
+(
+    router.register(r'books', BookViewSet)
+            .register(r'comments', BookCommentViewSet, 'books-comment', parents_query_lookups=['object_id'])
+)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
