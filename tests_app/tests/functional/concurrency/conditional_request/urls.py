371b2b97133c3fbf85b9f6ca30dbc22cb2390391
Philipp Kainz <phil@phils-probook.local>
2017-01-02 12:02:10 +0100

    added functional tests for @api_etag decorator and custom views

--- tests_app/tests/functional/concurrency/conditional_request/urls.py
+++ tests_app/tests/functional/concurrency/conditional_request/urls.py
@@ -1,7 +1,16 @@
+from django.conf.urls import url, include
 from rest_framework import routers
-from .views import BookViewSet
+from .views import BookViewSet, BookListCreateView, BookChangeView, BookCustomDestroyView
 
 router = routers.DefaultRouter()
 router.register(r'books', BookViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    # manually add endpoints for APIView instances
+    url(r'books_view/(?P<pk>[0-9]+)/custom_delete/', BookCustomDestroyView.as_view(), name='book_view-custom_delete'),
+    url(r'books_view/', BookListCreateView.as_view(), name='book_view-list'),
+    url(r'books_view/(?P<pk>[0-9]+)/', BookChangeView.as_view(), name='book_view-detail'),
+
+    # include the URLs from the default viewset
+    url(r'^', include(router.urls)),
+]
