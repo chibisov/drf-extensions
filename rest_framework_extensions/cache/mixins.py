3afd490340973e8f6f5275f088f9b793447d2cec
Marco Montanari <sirmmo@gmail.com>
2017-03-23 07:28:39 +0100

    timeout setting mixin

--- rest_framework_extensions/cache/mixins.py
+++ rest_framework_extensions/cache/mixins.py
@@ -8,16 +8,18 @@ class BaseCacheResponseMixin(object):
     # test_cache_reponse(view_instance, method, should_rebuild_after_method_evaluation)
     object_cache_key_func = extensions_api_settings.DEFAULT_OBJECT_CACHE_KEY_FUNC
     list_cache_key_func = extensions_api_settings.DEFAULT_LIST_CACHE_KEY_FUNC
+    object_cache_timeout = extensions_api_settings.DEFAULT_CACHE_RESPONSE_TIMEOUT
+    list_cache_timeout = extensions_api_settings.DEFAULT_CACHE_RESPONSE_TIMEOUT
 
 
 class ListCacheResponseMixin(BaseCacheResponseMixin):
-    @cache_response(key_func='list_cache_key_func')
+    @cache_response(key_func='list_cache_key_func', timeout='list_cache_timeout')
     def list(self, request, *args, **kwargs):
         return super(ListCacheResponseMixin, self).list(request, *args, **kwargs)
 
 
 class RetrieveCacheResponseMixin(BaseCacheResponseMixin):
-    @cache_response(key_func='object_cache_key_func')
+    @cache_response(key_func='object_cache_key_func', timeout='object_cache_timeout')
     def retrieve(self, request, *args, **kwargs):
         return super(RetrieveCacheResponseMixin, self).retrieve(request, *args, **kwargs)
 
