97490d4ea613190a8210a51c4a4253626074e843
Ivo <ivoscc@users.noreply.github.com>
2017-09-07 08:12:12 -0500

    Fix `lookup_value_regex` for nesting over two levels deep. (#192)
    Consider every parent's (not just the last one's) `lookup_value_regex` attribute
    when building the URL prefix.

--- tests_app/tests/unit/routers/nested_router_mixin/tests.py
+++ tests_app/tests/unit/routers/nested_router_mixin/tests.py
@@ -7,6 +7,9 @@ from .views import (
     UserViewSet,
     GroupViewSet,
     PermissionViewSet,
+    CustomRegexUserViewSet,
+    CustomRegexGroupViewSet,
+    CustomRegexPermissionViewSet,
 )
 
 
@@ -18,6 +21,21 @@ class NestedRouterMixinTest(APITestCase):
     def get_parent_lookup_regex(self, value):
         return get_lookup_allowed_symbols(compose_parent_pk_kwarg_name(value), force_dot=True)
         # return '(?P<{0}>[^/.]+)'.format(compose_parent_pk_kwarg_name(value))
+
+    def get_custom_regex_lookup(self, pk_kwarg_name, lookup_value_regex):
+        """ Build lookup regex with custom regular expression. """
+        return '(?P<{pk_kwarg_name}>{lookup_value_regex})'.format(
+            pk_kwarg_name=pk_kwarg_name,
+            lookup_value_regex=lookup_value_regex
+        )
+
+    def get_custom_regex_parent_lookup(self, parent_pk_kwarg_name,
+                                       parent_lookup_value_regex):
+        """ Build parent lookup regex with custom regular expression. """
+        return self.get_custom_regex_lookup(
+            compose_parent_pk_kwarg_name(parent_pk_kwarg_name),
+            parent_lookup_value_regex
+        )
 
     def test_one_route(self):
         router = ExtendedSimpleRouter()
@@ -110,5 +128,68 @@ class NestedRouterMixinTest(APITestCase):
                 self.get_parent_lookup_regex('group__user'),
                 self.get_parent_lookup_regex('group'),
                 self.get_lookup_regex('pk')
+            ),
+        )
+
+    def test_nested_route_depth_3_custom_regex(self):
+        """
+        Nested routes with over two level of depth should respect all parents'
+        `lookup_value_regex` attribute.
+        """
+        router = ExtendedSimpleRouter()
+        (
+            router.register(r'users', CustomRegexUserViewSet, 'user')
+                  .register(r'groups', CustomRegexGroupViewSet, 'users-group',
+                            parents_query_lookups=['user'])
+                  .register(r'permissions', CustomRegexPermissionViewSet,
+                            'users-groups-permission', parents_query_lookups=[
+                               'group__user',
+                               'group',
+                            ]
+                  )
+        )
+
+        # custom regex configuration
+        user_viewset_regex = CustomRegexUserViewSet.lookup_value_regex
+        group_viewset_regex = CustomRegexGroupViewSet.lookup_value_regex
+        perm_viewset_regex = CustomRegexPermissionViewSet.lookup_value_regex
+
+        # test user list
+        self.assertEqual(router.urls[0].name, 'user-list')
+        self.assertEqual(router.urls[0]._regex, r'^users/$')
+
+        # test user detail
+        self.assertEqual(router.urls[1].name, 'user-detail')
+        self.assertEqual(router.urls[1]._regex, r'^users/{0}/$'.format(
+            self.get_custom_regex_lookup('pk', user_viewset_regex))
+        )
+
+        # test users group list
+        self.assertEqual(router.urls[2].name, 'users-group-list')
+        self.assertEqual(router.urls[2]._regex, r'^users/{0}/groups/$'.format(
+                self.get_custom_regex_parent_lookup('user', user_viewset_regex)
+            )
+        )
+        # test users group detail
+        self.assertEqual(router.urls[3].name, 'users-group-detail')
+        self.assertEqual(router.urls[3]._regex, r'^users/{0}/groups/{1}/$'.format(
+                self.get_custom_regex_parent_lookup('user', user_viewset_regex),
+                self.get_custom_regex_lookup('pk', group_viewset_regex)
+            ),
+        )
+        # test users groups permission list
+        self.assertEqual(router.urls[4].name, 'users-groups-permission-list')
+        self.assertEqual(router.urls[4]._regex, r'^users/{0}/groups/{1}/permissions/$'.format(
+                self.get_custom_regex_parent_lookup('group__user', user_viewset_regex),
+                self.get_custom_regex_parent_lookup('group', group_viewset_regex),
+            )
+        )
+
+        # test users groups permission detail
+        self.assertEqual(router.urls[5].name, 'users-groups-permission-detail')
+        self.assertEqual(router.urls[5]._regex, r'^users/{0}/groups/{1}/permissions/{2}/$'.format(
+                self.get_custom_regex_parent_lookup('group__user', user_viewset_regex),
+                self.get_custom_regex_parent_lookup('group', group_viewset_regex),
+                self.get_custom_regex_lookup('pk', perm_viewset_regex)
             ),
         )
