24688ae302f4f9e62493aeea37d857924aceba4f
Asif Saifuddin Auvi <auvipy@gmail.com>
2018-03-08 23:19:33 +0600

    Fixed django 2.0 errors (#209)
    * fix url import error
    
    * fixed on_delete argument error
    
    * fixed noreversematch import issue
    
    * fixed on_delete position argument error
    
    * fixed on_delete position argument error on unit test
    
    * import from django.urls

--- tests_app/tests/functional/routers/extended_default_router/tests.py
+++ tests_app/tests/functional/routers/extended_default_router/tests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from django.core.urlresolvers import NoReverseMatch
 from django.test import override_settings
+from django.urls import NoReverseMatch
 
 from rest_framework_extensions.test import APITestCase
 
