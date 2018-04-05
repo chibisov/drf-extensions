a23611218cb67b69fabdb761f61cc5a0127bd057
Asif Saifuddin Auvi <auvipy@gmail.com>
2018-04-06 00:49:23 +0600

    Update old style middleware with mixin

--- tests_app/tests/functional/examples/etags/remove_etag_gzip_postfix/middleware.py
+++ tests_app/tests/functional/examples/etags/remove_etag_gzip_postfix/middleware.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    MiddlewareMixin = object
 
 
-class RemoveEtagGzipPostfix(object):
+class RemoveEtagGzipPostfix(MiddlewareMixin):
     def process_response(self, request, response):
         if response.has_header('ETag') and response['ETag'][-6:] == ';gzip"':
             response['ETag'] = response['ETag'][:-6] + '"'
