31a85830404dc1947a854d52c36e42624342543e
Ryan P Kilby <rpkilby@ncsu.edu>
2018-04-12 14:34:26 -0400

    Drop old trailing_slash compat (pre-DRF 2.4)

--- rest_framework_extensions/compat_drf.py
+++ rest_framework_extensions/compat_drf.py
@@ -5,14 +5,6 @@ versions of Django REST Framework.
 from rest_framework_extensions.utils import get_rest_framework_features
 
 
-def add_trailing_slash_if_needed(regexp_string):
-    # todo: test me
-    if get_rest_framework_features()['router_trailing_slash']:
-        return regexp_string[:-2] + '{trailing_slash}$'
-    else:
-        return regexp_string
-
-
 def get_lookup_allowed_symbols(kwarg_name='pk', force_dot=False):
     # todo: test me
     if get_rest_framework_features()['use_dot_in_lookup_regex_by_default'] or force_dot:
