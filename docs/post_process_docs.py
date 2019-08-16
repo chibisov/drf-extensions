c412e14adbdbffff5c2fc37fca128e909888e826
Serhii Romanov <sromanov@sharpminds.com>
2019-08-16 12:53:41 +0300

    Update etag docs (see #217) Get rid of Yandex Metrica in the bottom of doc page

--- docs/post_process_docs.py
+++
@@ -1,5 +0,0 @@
-docs_file_path = 'docs/index.html'
-docs_file_content = open(docs_file_path, 'r', encoding='utf8').read()[:-len('</html>')]
-docs_file = open(docs_file_path, 'w', encoding='utf-8')
-metrika_file_content = open('docs/metrika_code.txt', 'r', encoding='utf-8').read()
-docs_file.write(docs_file_content + metrika_file_content + '</html>')
