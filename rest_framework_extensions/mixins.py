16751da0a181380b5cc2dcaabb3d31ed4b3039e0
zhibing.chen <87839912+sobadgirl@users.noreply.github.com>
2022-04-24 18:34:33 +0800

    feat: add ownership check when create and update

--- rest_framework_extensions/mixins.py
+++ rest_framework_extensions/mixins.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ValidationError
 # from rest_framework_extensions.etag.mixins import ReadOnlyETAGMixin, ETAGMixin
 from rest_framework_extensions.bulk_operations.mixins import ListUpdateModelMixin, ListDestroyModelMixin
 from rest_framework_extensions.settings import extensions_api_settings
+from rest_framework import status, exceptions
 from django.http import Http404
 from django.shortcuts import get_object_or_404 as _get_object_or_404
 
@@ -64,6 +65,29 @@ class PaginateByMaxMixin:
 
 class NestedViewSetMixin:
     parent_viewsets = set()
+
+    def check_ownership(self, serializer):
+        parent_query_dicts = self.get_parents_query_dict()
+        if parent_query_dicts:
+            parent_name, parent_value = list(parent_query_dicts.items())[-1]
+            items = serializer.validated_data
+            if not isinstance(items, list):
+                items = [items]
+            for item in items:
+                if item.get(parent_name, None) is None:
+                    raise exceptions.PermissionDenied(
+                        detail=f"You must specific '{parent_name}'", code=status.HTTP_403_FORBIDDEN)
+                if item.get(parent_name, None) != parent_value:
+                    raise exceptions.PermissionDenied(
+                        detail=f"You don't have permission to operate item that belone to '{parent_name}:{parent_value}'", code=status.HTTP_403_FORBIDDEN)
+
+    def perform_create(self, serializer):
+        self.check_ownership(serializer)
+        super().perform_create(serializer)
+
+    def perform_update(self, serializer):
+        self.check_ownership(serializer)
+        super().perform_update(serializer)
 
     def check_parent_object_permissions(self, request):
         # if parent viewset haven't init yet, then will raise no "kwargs" attribute error, but it doesn't matter, just ignore
