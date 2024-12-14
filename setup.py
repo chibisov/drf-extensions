75dcfdeaa525a7e35cff374bfa2a92319a16e60c
MehrazRumman <stardark394@gmail.com>
2024-12-14 20:28:12 +0600

    support added for python 3.11

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
