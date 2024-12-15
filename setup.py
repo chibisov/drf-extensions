09512be071a19eba28fa3c0d6fdd6e80ce7befed
Mehraz Hossain Rumman <59512321+MehrazRumman@users.noreply.github.com>
2024-12-15 12:40:54 +0600

    Python support added for 3.9, 3.10, 3.11, 3.12 (#348)
    * unit test fixed
    
    * testserialisers file txt added in the gitignore
    
    * testserialisers file txt added in the gitignore
    
    * test txt files deleted
    
    * Update tests_app/tests/functional/routers/nested_router_mixin/views.py
    
    * support for python 3.9 added
    
    * test files deleted
    
    * python 3.9 and 3.10 support added
    
    * support added for python 3.11
    
    * support for  python 3.12 added
    
    * python 3.10-3.12 added in github/workflows
    
    * python 3.10-3.12 added in github/workflows
    
    * remove python versions from travis.yml
    
    ---------
    
    Co-authored-by: Mehraz Hossian Rumman <rumman@adfinix.com>
    Co-authored-by: Asif Saif Uddin <auvipy@gmail.com>

--- setup.py
+++ setup.py
@@ -73,6 +73,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
