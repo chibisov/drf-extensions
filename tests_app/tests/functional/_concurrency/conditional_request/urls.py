c40977e8802d5ca6c2a50820850f84050877e745
Mehraz Hossain Rumman <59512321+MehrazRumman@users.noreply.github.com>
2024-12-15 21:02:03 +0600

    Django 4.2 support added for python 3.9 to 3.12 & for drf 3.13 (#349)
    * unit test fixed
    
    * testserialisers file txt added in the gitignore
    
    * testserialisers file txt added in the gitignore
    
    * test txt files deleted
    
    * Update tests_app/tests/functional/routers/nested_router_mixin/views.py
    
    * support for python 3.9 added
    
    * test files deleted
    
    * python 3.9 and 3.10 support added
    
    * support added for python 3.11
    
    * support for  python 3.12 added
    
    * python 3.10-3.12 added in github/workflows
    
    * python 3.10-3.12 added in github/workflows
    
    * remove python versions from travis.yml
    
    * django 4.2 support added for python 3.9 to 3.12 and for drf 3.14
    
    * readme updated
    
    ---------
    
    Co-authored-by: Mehraz Hossian Rumman <rumman@adfinix.com>
    Co-authored-by: Asif Saif Uddin <auvipy@gmail.com>

--- tests_app/tests/functional/_concurrency/conditional_request/urls.py
+++ tests_app/tests/functional/_concurrency/conditional_request/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.urls import re_path, include
 from rest_framework import routers
 from .views import (BookViewSet, BookListCreateView, BookChangeView, BookCustomDestroyView,
                     BookUnconditionalDestroyView, BookUnconditionalUpdateView)
@@ -8,14 +8,14 @@ router.register(r'books', BookViewSet)
 
 urlpatterns = [
     # manually add endpoints for APIView instances
-    url(r'books_view/(?P<pk>[0-9]+)/custom/delete/', BookCustomDestroyView.as_view(), name='book_view-custom_delete'),
-    url(r'books_view/(?P<pk>[0-9]+)/unconditional/delete/', BookUnconditionalDestroyView.as_view(),
+    re_path(r'books_view/(?P<pk>[0-9]+)/custom/delete/', BookCustomDestroyView.as_view(), name='book_view-custom_delete'),
+    re_path(r'books_view/(?P<pk>[0-9]+)/unconditional/delete/', BookUnconditionalDestroyView.as_view(),
         name='book_view-unconditional_delete'),
-    url(r'books_view/(?P<pk>[0-9]+)/unconditional/update/', BookUnconditionalUpdateView.as_view(),
+    re_path(r'books_view/(?P<pk>[0-9]+)/unconditional/update/', BookUnconditionalUpdateView.as_view(),
         name='book_view-unconditional_update'),
-    url(r'books_view/', BookListCreateView.as_view(), name='book_view-list'),
-    url(r'books_view/(?P<pk>[0-9]+)/', BookChangeView.as_view(), name='book_view-detail'),
+    re_path(r'books_view/', BookListCreateView.as_view(), name='book_view-list'),
+    re_path(r'books_view/(?P<pk>[0-9]+)/', BookChangeView.as_view(), name='book_view-detail'),
 
     # include the URLs from the default viewset
-    url(r'^', include(router.urls)),
+    re_path(r'^', include(router.urls)),
 ]
--- tests_app/tests/functional/_examples/etags/remove_etag_gzip_postfix/urls.py
+++ tests_app/tests/functional/_examples/etags/remove_etag_gzip_postfix/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import MyView
 
 
 urlpatterns = [
-    url(r'^remove-etag-gzip-postfix/$', MyView.as_view()),
+    re_path(r'^remove-etag-gzip-postfix/$', MyView.as_view()),
 ]
