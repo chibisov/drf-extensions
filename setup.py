4b53cfe13c0b9eb16e7d9e81e2438aba8d4d0b80
Clement Cavanier <clement.cavanier@bleemeo.com>
2025-12-05 14:05:42 +0100

    use python/django matrix compatibility in tox.ini for github ci

--- setup.py
+++ setup.py
@@ -82,6 +82,8 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
